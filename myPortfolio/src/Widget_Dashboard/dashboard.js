8f3f4ac47297b80e27e4583ca42dfcf1e866f8af
Joseph William Hauger <joseph>
2019-03-05 20:55:09 -0600

    geolocation added

---
+++ myPortfolio/src/Widget_Dashboard/dashboard.js
@@ -0,0 +1,31 @@
+function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#mapLink');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.gps-coordinates.net/`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+  document.querySelector('#findMe').addEventListener('click', geoFindMe);
