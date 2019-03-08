3e5c0f37cdf3967d630acd66f349fd49d17294ba
Joseph William Hauger <joseph>
2019-03-07 20:52:45 -0600

    lots of updates

--- myPortfolio/src/Tic-Tac-Toe/Tic_Tac.js
+++ myPortfolio/src/Tic-Tac-Toe/Tic_Tac.js
@@ -1,3 +1,4 @@
+//this is outlining my board
 var origBoard;
 const huPlayer = '0';
 const aiPlayer = 'X';
@@ -11,9 +12,11 @@ const winCombos = [
     [0, 4, 8],
     [6, 4, 2]
 ]
+//this is saying at the start of each game sellect all the cells counted in the above
 const cells = document.querySelectorAll('.cell');
 startGame();
-
+//this is saying what happens each time a new game starts it also reset the board to be blank
+//it clears the cells and background color and looks to see if someone needs to take a turn
 function startGame() {
     document.querySelector(".endgame").style.display = "none";
     origBoard = Array.from(Array(9).keys());
@@ -23,21 +26,28 @@ function startGame() {
         cells[i].addEventListener('click', turnClick, false);
     }
 }
-
+//this is saying when someone takes a turn (the human or the computer), did someone win?  is there a tie?
+//the "number" is replaced by an X or O once clicked and this is saying if it is a number...then you can click there.
+//If it is not a number then you can't click there.
 function turnClick (square) {
     if (typeof origBoard[square.target.id] == 'number') {
     turn(square.target.id, huPlayer);
     if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
     }
 }
-
+//this shows who took a turn and shows the element that was clicked so the x or o shows
+//it again checks to see if someone won the game then the game will end.
 function turn(squareId, player) {
     origBoard[squareId] = player;
     document.getElementById(squareId).innerText = player;
     let gameWon = checkWin(origBoard, player);
     if (gameWon) gameOver(gameWon);
 }
-
+//this is explaining how to check if the game has been won
+//it reviews all locations on teh board via reducing them and giving the value.
+//so it is showing all indexes the player has played in.
+//it searches using hte wincombos to see if one of the winning ways has been played
+//meaning every time somone takes a turn it loops through all the wincombos to see if it is a win and which player it is.
 function checkWin(board, player) {
     let plays = board.reduce((a, e, i) =>
     (e === player) ? a.concat(i) : a, []);
@@ -48,32 +58,40 @@ function checkWin(board, player) {
             break;
         }
     }
+    //if the loops show a win then 
     return gameWon;
 }
-
+//if the game is won this is the function as to what to do.
+//it will set background colors as blue or red of the winning squares
 function gameOver(gameWon) {
     for (let index of winCombos[gameWon.index]){
         document.getElementById(index).style.backgroundColor = 
                 gameWon.player == huPlayer ? "blue" : "red";
     }
+    //this makes it not possible to click on any cells if the game has been won and is over.
+    //plus declare the winner
     for (var i = 0; i < cells.length; i++) {
         cells[i].removeEventListener('click', turnClick, false);
     }
     declareWinner(gameWon.player == huPlayer ? "You win!" : "You Lose.");
 }
-
+//here on down it got VERY challenging but after around 30 trys I started to understand.
+//this is the endgame function of saying you won or lost
 function declareWinner(who) {
     document.querySelector(".endgame").style.display = "block";
     document.querySelector(".endgame .text").innerText = who;
 }
-
+//this just tells the computer to play the next empty square
+//note "number" like up on the turn click
 function emptySquares() {
     return origBoard.filter(s => typeof s == 'number');
 }
+//this is tell the AI player to determine place to play
 function bestSpot() {
     return minimax(origBoard, aiPlayer).index;
 }
-
+//determines if there are no more possible places to play and no one has won.
+//it will also change the background color, declare the status and prevent further clicks
 function checkTie() {
     if (emptySquares().length == 0) {
         for (var i = 0; i < cells.length; i++) {
