befb45515d861a650d3933f5d230965f083bed3f
Joseph William Hauger <joseph>
2019-03-14 16:01:27 -0500

    gong!

--- myPortfolio/src/GONG!/Gong.js
+++ myPortfolio/src/GONG!/Gong.js
@@ -1,12 +1,21 @@
 //function to make the gong sound!
 function play() {
-    console.log("clicked")
     var audio = document.getElementById("gonged");
     audio.play();
-
-    function count(){
-        
-    }
-
 }
+//function to cound each time the gong is played
+function displaycount(){
+    var count = document.getElementsByClassName("status");
+    count = 0;
+    return count.innerHTML = + 1;
+ }
 
+
+
+//function at 20 gongs audio of bruce lee "woooawoo"
+
+//function at 50 gongs audio of "heavenly glory"
+
+//function at 75 gongs audio of yoga
+
+//100 gongs entire page become chakra picture with circling oms audio
