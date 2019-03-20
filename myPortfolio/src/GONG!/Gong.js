86be0fd4e74b29167973ac797e7365ddb882cc81
Joseph William Hauger <joseph>
2019-03-19 19:41:21 -0500

    more changes

--- myPortfolio/src/GONG!/Gong.js
+++ myPortfolio/src/GONG!/Gong.js
@@ -16,28 +16,23 @@ function play() {
     document.getElementById("status").innerHTML = count();
     
        switch (true) {
+           //function at 20 gongs audio of monks chanting
            case (document.getElementById("status").innerHTML == 20):
                var audio = document.getElementById("ya");
                 audio.play();
                 break;
+            //function at 50 gongs audio of Bruce Lee advice    
             case (document.getElementById("status").innerHTML == 50):
                 var audio = document.getElementById("heaven");
                 audio.play();
                 break;
+            //function at 75 gongs audio of yoda    
             case (document.getElementById("status").innerHTML == 75):
                 var audio = document.getElementById("force");
                 audio.play();
                 break;
+            //100 gongs new page become chakra picture with circling oms audio    
             case (document.getElementById("status").innerHTML > 99):  
                 window.open("./Buddha.html");    
         }
 }
-
-
-//function at 20 gongs audio of bruce lee "woooawoo"
-
-//function at 50 gongs audio of "heavenly glory"
-
-//function at 75 gongs audio of yoga
-
-//100 gongs entire page become chakra picture with circling oms audio
